f26daf7d97c632303524f798f9df0e96e1949cd3
roldophe <138679281+roldophe@users.noreply.github.com>
2023-12-06 11:27:39 +0700

    Select a json from objects

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -9,10 +9,12 @@ import { Room, RoomList } from './rooms';
 export class RoomsComponent implements OnInit {
   birthday = new Date(1988, 3, 15);
 
-  hotelName = 'TL Hotel';
+  hotelName = 'TL ROOMS';
   numberOfRooms = 10;
 
   hideRooms = false;
+
+  selectedRoom !:RoomList;
   roomList: RoomList[] = [];
 
   constructor() { }
@@ -24,39 +26,43 @@ export class RoomsComponent implements OnInit {
   ngOnInit(): void { 
     this.roomList=[
       {
-        roomNumber: 105,
-        roomType: "Oral Room",
-        amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
-        price: 500,
-        photos: "https://bowa.com/wp-content/uploads/2015/07/MAY-Potomac-MD-Whole-House-Renovation-Sitting-Room.jpg",
-        checkinTime: new Date('11-Nov-2023'),
-        checkoutTime: new Date('11-Nov-2024'),
-        rating:4.55
-      },
-      {
-        roomNumber: 101,
-        roomType: "Duluxe Room",
-        amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
-        price: 1000,
-        photos: "https://th.bing.com/th/id/OIP.YTGQcpOpfSPfAqBj6aTB2wHaE8?rs=1&pid=ImgDetMain",
-        checkinTime: new Date('19-Nov-2023'),
-        checkoutTime: new Date('19-Nov-2024'),
-        rating:2.46
-      },
-      {
-        roomNumber: 205,
-        roomType: "Liquid Room",
-        amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
-        price: 600,
-        photos: "https://th.bing.com/th/id/OIP.tVyVUZKDKZG90vLtbKZwwgHaE8?w=1500&h=1001&rs=1&pid=ImgDetMain",
-        checkinTime: new Date('12-Nov-2023'),
-        checkoutTime: new Date('16-Nov-2024'),
-        rating:2.34
-      },
+      roomNumber: 105,
+      roomType: "Oral Room",
+      amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
+      price: 500,
+      photos: "https://bowa.com/wp-content/uploads/2015/07/MAY-Potomac-MD-Whole-House-Renovation-Sitting-Room.jpg",
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date('11-Nov-2024'),
+      rating:4.55
+    },
+    {
+      roomNumber: 101,
+      roomType: "Duluxe Room",
+      amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
+      price: 1000,
+      photos: "https://th.bing.com/th/id/OIP.YTGQcpOpfSPfAqBj6aTB2wHaE8?rs=1&pid=ImgDetMain",
+      checkinTime: new Date('19-Nov-2023'),
+      checkoutTime: new Date('19-Nov-2024'),
+      rating:2.46
+    },
+    {
+      roomNumber: 205,
+      roomType: "Liquid Room",
+      amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
+      price: 600,
+      photos: "https://th.bing.com/th/id/OIP.tVyVUZKDKZG90vLtbKZwwgHaE8?w=1500&h=1001&rs=1&pid=ImgDetMain",
+      checkinTime: new Date('12-Nov-2023'),
+      checkoutTime: new Date('16-Nov-2024'),
+      rating:2.34
+    },
     ]
   }
 
   toggle() {
     this.hideRooms = !this.hideRooms
+  }
+  selectRoom(room:RoomList){
+    // console.log(room)
+    this.selectedRoom=room;
   }
 }
