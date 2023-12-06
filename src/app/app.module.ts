306c115fd75825a46d3529963080be24ab8479ce
roldophe <138679281+roldophe@users.noreply.github.com>
2023-12-06 09:56:48 +0700

    Assign data into function ngOnInit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { RoomsComponent } from './rooms/rooms.component';
     RoomsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -13,38 +13,7 @@ export class RoomsComponent implements OnInit {
   numberOfRooms = 10;
 
   hideRooms = false;
-  roomList: RoomList[] = [
-    {
-      roomNumber: 105,
-      roomType: "Oral Room",
-      amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
-      price: 500,
-      photos: "https://bowa.com/wp-content/uploads/2015/07/MAY-Potomac-MD-Whole-House-Renovation-Sitting-Room.jpg",
-      checkinTime: new Date('11-Nov-2023'),
-      checkoutTime: new Date('11-Nov-2024'),
-      rating:4.55
-    },
-    {
-      roomNumber: 101,
-      roomType: "Duluxe Room",
-      amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
-      price: 1000,
-      photos: "https://th.bing.com/th/id/OIP.YTGQcpOpfSPfAqBj6aTB2wHaE8?rs=1&pid=ImgDetMain",
-      checkinTime: new Date('19-Nov-2023'),
-      checkoutTime: new Date('19-Nov-2024'),
-      rating:2.46
-    },
-    {
-      roomNumber: 205,
-      roomType: "Liquid Room",
-      amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
-      price: 600,
-      photos: "https://th.bing.com/th/id/OIP.tVyVUZKDKZG90vLtbKZwwgHaE8?w=1500&h=1001&rs=1&pid=ImgDetMain",
-      checkinTime: new Date('12-Nov-2023'),
-      checkoutTime: new Date('16-Nov-2024'),
-      rating:2.34
-    },
-  ]
+  roomList: RoomList[] = [];
 
   constructor() { }
   rooms: Room = {
@@ -52,7 +21,40 @@ export class RoomsComponent implements OnInit {
     availableRooms: 10,
     bookedRooms: 5
   }
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.roomList=[
+      {
+        roomNumber: 105,
+        roomType: "Oral Room",
+        amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
+        price: 500,
+        photos: "https://bowa.com/wp-content/uploads/2015/07/MAY-Potomac-MD-Whole-House-Renovation-Sitting-Room.jpg",
+        checkinTime: new Date('11-Nov-2023'),
+        checkoutTime: new Date('11-Nov-2024'),
+        rating:4.55
+      },
+      {
+        roomNumber: 101,
+        roomType: "Duluxe Room",
+        amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
+        price: 1000,
+        photos: "https://th.bing.com/th/id/OIP.YTGQcpOpfSPfAqBj6aTB2wHaE8?rs=1&pid=ImgDetMain",
+        checkinTime: new Date('19-Nov-2023'),
+        checkoutTime: new Date('19-Nov-2024'),
+        rating:2.46
+      },
+      {
+        roomNumber: 205,
+        roomType: "Liquid Room",
+        amenities: "Air Conditioner, Free Wi-Fi, Tv, Bathroom, Kitchen",
+        price: 600,
+        photos: "https://th.bing.com/th/id/OIP.tVyVUZKDKZG90vLtbKZwwgHaE8?w=1500&h=1001&rs=1&pid=ImgDetMain",
+        checkinTime: new Date('12-Nov-2023'),
+        checkoutTime: new Date('16-Nov-2024'),
+        rating:2.34
+      },
+    ]
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms
