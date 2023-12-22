7d1542d25d1a1cd3895ba0fb04f40c3cd1036d95
roldophe <138679281+roldophe@users.noreply.github.com>
2023-12-22 11:00:38 +0700

    login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
-  { path: 'employee', component: EmployeeComponent },
+  { path: 'employee', component: EmployeeComponent, canActivate: [LoginGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'rooms',
     loadChildren: () =>
       import('./rooms/rooms.module').then((m) => m.RoomsModule),
+      canActivate: [LoginGuard],
   },
-  { path: 'booking', loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule) },
+  {
+    path: 'booking',
+    loadChildren: () =>
+      import('./booking/booking.module').then((m) => m.BookingModule),
+    canActivate: [LoginGuard],
+  },
   { path: '**', component: NotFoundComponent }, // Optional: Handle invalid routes
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { ContainerComponent } from './container/container.component';
 import { Component, OnInit, ViewChild, ViewContainerRef, AfterViewInit, ElementRef, Inject } from '@angular/core';
 import { RoomsComponent } from './rooms/rooms.component';
-import { localStorageToken } from './localstorage.token';
 import { ConfigService } from './services/config.service';
+import { NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { localStorageToken } from './localstorage.token';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +15,22 @@ export class AppComponent implements OnInit {
   title = 'hotelinventoryapp';
   @ViewChild('name', { static: true }) name!: ElementRef;
 
+ 
+  constructor(@Inject(localStorageToken) 
+  private localStorage:any,
+  private configService:ConfigService,
+  private router: Router){
+
+  }
+
   ngOnInit(): void {
     //this.name.nativeElement.innerText = "TL Hotel";
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart)
+    ).subscribe((event) => {
+      console.log('NavigationStarted', event);
+    })
   }
-  constructor(@Inject(localStorageToken) private localStorage:any,
-  private configService:ConfigService){
-
-  }
-
   // role = 'Admin';
   // @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef
   // ngAfterViewInit(): void {
