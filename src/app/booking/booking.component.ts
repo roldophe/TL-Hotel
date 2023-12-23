16d580257957c44df0acaa529bdb26adc0380ed7
roldophe <138679281+roldophe@users.noreply.github.com>
2023-12-23 11:35:02 +0700

    Add Dynamic formControl

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from './../services/config.service';
 import { Component, OnInit } from '@angular/core';
 import {
+  FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -14,6 +15,9 @@ import {
 })
 export class BookingComponent implements OnInit {
   bookingForm!: FormGroup;
+  get guests() {
+    return this.bookingForm.get('guests') as FormArray;
+  }
   constructor(
     private configService: ConfigService,
     private fb: FormBuilder,
@@ -21,7 +25,7 @@ export class BookingComponent implements OnInit {
 
   ngOnInit(): void {
     this.bookingForm = this.fb.group({
-      roomId: new FormControl({value:'2',disabled:true}), //new FormControl('1') => Can change value
+      roomId: new FormControl({ value: '2', disabled: true }), //new FormControl('1') => Can change value
       guestEmail: [''],
       checkinDate: [''],
       checkoutDate: [''],
@@ -38,7 +42,12 @@ export class BookingComponent implements OnInit {
         country: [''],
         zipCode: [''],
       }),
-
+      guests: this.fb.array([
+        this.fb.group({
+          guestName: [''],
+          age: new FormControl(''),
+        }),
+      ]),
       guestCount: [''],
     });
   }
@@ -54,5 +63,22 @@ export class BookingComponent implements OnInit {
   }
   addBooking() {
     console.log('addBooking', this.bookingForm.getRawValue()); // TODO: check all fields with valid and disabled values value
+  }
+
+  addGuest() {
+    this.guests.push(
+      this.fb.group({
+        guestName: [''],
+        age: new FormControl(''),
+      }),
+    );
+  }
+  addPassport() {
+    this.bookingForm.addControl('passport', new FormControl(''));
+  }
+  deletePassport() {
+    if(this.bookingForm.get('passport')) {
+      this.bookingForm.removeControl('passport');
+    }
   }
 }
